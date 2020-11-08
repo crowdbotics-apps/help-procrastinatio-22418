3c952b786054c832b6fbc8af94660dd68b5da8b1
Crowdbotics <team@crowdbotics.com>
2020-11-08 23:44:59 +0000

    Update models

--- backend/help_procrastinatio_22418/wsgi.py
+++ backend/help_procrastinatio_22418/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'help_procrastinatio_22418.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "help_procrastinatio_22418.settings")
 
 application = get_wsgi_application()
